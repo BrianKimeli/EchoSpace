ecf22493837084ab52978449d1c7947784b9af4a
BRIAN KIMELI <115707322+BrianKimeli@users.noreply.github.com>
2025-09-04 08:22:03 +0300

    Fix EchoSpace Errors

--- src/components/hateWords.js
+++
@@ -1,9 +0,0 @@
-export const hatedWords = [
-  'hate', 'kill', 'destroy', 'violent',
-  'racist', 'bigot', 'sexist', 'nazi',
-  'fascist', 'terrorist', 'whore', 'slut',
-  'suicide', 'self-harm', 'abuse', 'die',
-  'shoot', 'dead', 'murder', 'bomb'
-];
-
-export default hatedWords;
